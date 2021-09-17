08e3a682399fba57811cab0f34b1969f6b9b8109
fatgue <no1torra@gmail.com>
2021-09-17 11:04:09 +0200

    init

---
+++ Alamofire/Alamofire.h
@@ -0,0 +1,18 @@
+//
+//  Alamofire.h
+//  Alamofire
+//
+//  Created by Fatih Gürbüz on 17.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Alamofire.
+FOUNDATION_EXPORT double AlamofireVersionNumber;
+
+//! Project version string for Alamofire.
+FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alamofire/PublicHeader.h>
+
+
